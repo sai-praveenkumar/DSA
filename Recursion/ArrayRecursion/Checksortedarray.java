7d7e9ded949969b43725c2af234340395c0454c5
SaiPraveenKumar <saipraveenk816@gmail.com>
2023-11-30 18:59:16 +0530

    new changes made

---
+++ Recursion/ArrayRecursion/Checksortedarray.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class CheckSortedArray {
+	public static void main(String[] args) {
+		int[] arr= {2,3,4,6,8,12,15,35,132,21};
+		boolean check=checksorted(arr,0);
+		System.out.println(check);
+	}
+	public static boolean checksorted(int[] arr,int curr) {
+		if(curr==arr.length-1) {
+			return true;
+		}
+		return arr[curr]< arr[curr+1] && checksorted(arr,curr+1);
+	}
+}
