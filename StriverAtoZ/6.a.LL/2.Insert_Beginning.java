941f9dda4635e56af77d84c002c309e568b89327
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-01-10 20:32:42 +0530

    new changes made

--- StriverAtoZ/6.a.LL/2.Insert_Beginning.java
+++ StriverAtoZ/6.a.LL/2.Insert_Beginning.java
@@ -5,39 +5,40 @@
  class Node {
      public int data;
      public Node next;
+     public Node prev;
 
      Node()
      {
          this.data = 0;
          this.next = null;
+         this.prev = null;
      }
 
      Node(int data)
      {
          this.data = data;
          this.next = null;
+         this.prev = null;
      }
 
      Node(int data, Node next)
      {
          this.data = data;
          this.next = next;
+         this.prev = next;
      }
  };
 
  *****************************************************************/
 
- public class Solution {
-    public static Node constructLL(int []arr) {
-        Node head=new Node(arr[0]);
-        Node move=head;
-        for(int i=1;i<arr.length;i++){
-            Node temp =new Node(arr[i]);
-            move.next=temp;
-            move = temp;
-        }
-        return head;
-    }
-}
+ public class Solution
+ {
+     public static Node insertAtFirst(Node list, int newValue) {
+         Node head=new Node(newValue);
+         head.next= list;
+         list=head;
+         return list;
+     }
+ }
 
-// Problem Link : https://tinyurl.com/55cy4jtd
+ //Problem Link : https://www.codingninjas.com/studio/problems/insert-node-at-the-beginning_8144739?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
