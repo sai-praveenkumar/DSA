4488fec2ef7d0231f4ac9081901d6d2cb5a1a0d7
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-01-10 20:22:07 +0530

    new changes made

--- StriverAtoZ/6.a.LL/2.Insert_First.java
+++ StriverAtoZ/6.a.LL/2.Insert_First.java
@@ -40,4 +40,5 @@
     }
 }
 
-// Problem Link : https://tinyurl.com/55cy4jtd
+
+// link : https://www.codingninjas.com/studio/problems/insert-node-at-the-beginning_8144739?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
