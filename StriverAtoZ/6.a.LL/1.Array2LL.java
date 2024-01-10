157873e05ce21440f524fc0753d549e94771fc45
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-01-10 20:09:39 +0530

    new changes made

---
+++ StriverAtoZ/6.a.LL/1.Array2LL.java
@@ -0,0 +1,41 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+ public class Solution {
+    public static Node constructLL(int []arr) {
+        Node head=new Node(arr[0]);
+        Node move=head;
+        for(int i=1;i<arr.length;i++){
+            Node temp =new Node(arr[i]);
+            move.next=temp;
+            move = temp;
+        }
+        return head;
+    }
+}
