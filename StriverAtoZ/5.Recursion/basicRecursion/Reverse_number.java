de5e6cac141eab1452267f3e08716e06407fdcea
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-06-02 10:33:01 +0530

    new challenges added

--- StriverAtoZ/5.Recursion/basicRecursion/Reverse_number.java
+++ StriverAtoZ/5.Recursion/basicRecursion/Reverse_number.java
@@ -1,14 +1,33 @@
-import java.util.*;
-class Reverse_Number{
-    public static void main(String[] args){
-        int number = 4567;
-        System.out.println(rev(number,0));
+//leetcode : 7
+class Solution {
+    public int reverse(int x) {
+        int num = Math.abs(x);
+        int rev = 0;
+        while (num!=0){
+            int rem = num%10;
+            if(rev > (Integer.MAX_VALUE - rem)/10)
+            return 0;
+            rev = rev*10 + rem;
+            num /=10;
+        }
+        return (x > 0) ? rev : -rev;
     }
-    private static int rev(int n,int sum){
-        if(n==0)
-        return sum;
-        int rem=n%10;
-        sum = sum*10+rem;
-        return rev(n/10,sum);
+}
+
+// Using recursion
+class Solution {
+    public int reverse(int x) {
+        int num= Math.abs(x);
+        int rev=0;
+        rev=rever(num,rev);
+        return (x>0) ? rev : -rev;
+    }
+    public int rever(int num,int rev){
+        if(num==0) return rev;
+        int rem=num%10;
+        if(rev > (Integer.MAX_VALUE-rem)/10) return 0;
+        rev=rev*10+rem;
+        num/=10;
+        return rever(num,rev);
     }
 }
