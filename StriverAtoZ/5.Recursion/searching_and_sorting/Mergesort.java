897c5ae9d931a28064e2de3a47f6147c1cec00d9
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-06-07 22:36:53 +0530

    merge sort

--- StriverAtoZ/5.Recursion/searching_and_sorting/Mergesort.java
+++ StriverAtoZ/5.Recursion/searching_and_sorting/Mergesort.java
@@ -40,3 +40,93 @@ public class Mergesort{
     }
 }
 
+// Merge Sort Inplace 
+
+// Problem link : https://www.geeksforgeeks.org/problems/merge-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=merge-sort
+
+// Using array as Auxilary space
+class Solution {
+    private static void merge(int[] arr, int low, int mid, int high) {
+       int[] temp= new int[high-low+1];
+       int left=low;
+       int right=mid+1;
+       int k=0;
+       while (left <= mid && right <= high) {
+           if (arr[left] <= arr[right]) {
+               temp[k]=arr[left];
+               left++;
+           } 
+           else {
+               temp[k]=arr[right];
+               right++;
+           }
+           k++;
+       }
+
+       while (left <= mid) {
+           temp[k]=arr[left];
+           left++;
+           k++;
+       }
+
+       while (right <= high) {
+           temp[k]=arr[right];
+           right++;
+           k++;
+       }
+
+       for (int i = low; i <= high; i++) {
+           arr[i] = temp[i - low];
+       }
+   }
+
+   public static void mergeSort(int[] arr, int low, int high) {
+       if (low >= high) return;
+       int mid = (low + high) / 2 ;
+       mergeSort(arr, low, mid);  
+       mergeSort(arr, mid + 1, high); 
+       merge(arr, low, mid, high);  
+   }
+}
+
+// Using ArrayList as an auxilary space
+
+class Solution {
+    private static void merge(int[] arr, int low, int mid, int high) {
+       ArrayList<Integer> temp = new ArrayList<>(); 
+       int left = low;      
+       int right = mid + 1;   
+
+       while (left <= mid && right <= high) {
+           if (arr[left] <= arr[right]) {
+               temp.add(arr[left]);
+               left++;
+           } else {
+               temp.add(arr[right]);
+               right++;
+           }
+       }
+
+       while (left <= mid) {
+           temp.add(arr[left]);
+           left++;
+       }
+
+       while (right <= high) {
+           temp.add(arr[right]);
+           right++;
+       }
+
+       for (int i = low; i <= high; i++) {
+           arr[i] = temp.get(i - low);
+       }
+   }
+
+   public static void mergeSort(int[] arr, int low, int high) {
+       if (low >= high) return;
+       int mid = (low + high) / 2 ;
+       mergeSort(arr, low, mid);  
+       mergeSort(arr, mid + 1, high); 
+       merge(arr, low, mid, high);  
+   }
+}
