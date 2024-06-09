329a7c209616ff4a668db5210e179a87849de566
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-06-09 20:48:07 +0530

    new changes made

--- StriverAtoZ/3.Arrays/easy/9.MissingNumber.java
+++ StriverAtoZ/3.Arrays/easy/9.MissingNumber.java
@@ -9,4 +9,19 @@ class Solution {
     }
 }
 
+// Approach 2 : Using XOR operations
+// Logic : XOR of something with 0 wil return the same number. ex:  5^0 = 5
+//        : XOR of 2 same numbers will return 0. ex : 5^5= 0
+//        : XORing 2 different numbers will result nothing ex :4^5= 5^4  (Order also doesn't matter in XOR operations)
+class Solution {
+    public int missingNumber(int[] nums) {
+       int sum = 0;
+       for(int i=1;i<=nums.length;i++)
+            sum^=i;
+        for(int i=0;i<nums.length;i++)
+            sum^=nums[i];
+        return sum;
+
+    }
+}
 // Follow up Q : very imp
