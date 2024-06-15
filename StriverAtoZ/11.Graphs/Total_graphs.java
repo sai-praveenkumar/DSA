de1ed66072ae2df65e1174830a53645bb399625e
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-06-15 15:34:25 +0530

    new changes made

--- StriverAtoZ/11.Graphs/Total_graphs.java
+++ StriverAtoZ/11.Graphs/Total_graphs.java
@@ -1,3 +1,5 @@
+// Problem link : https://www.geeksforgeeks.org/problems/graph-and-vertices/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=graph-and-vertices
+
 class Solution {
     static long count(int n) {
     return (long)Math.pow(2,n*(n-1)/2);
