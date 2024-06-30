baa28211cd9967129c6653f8148b1fcbaa699d23
SaiPraveenKumar Jallipalli <91360085+sai-praveenkumar@users.noreply.github.com>
2024-06-30 16:06:35 +0530

    Update Selectionsort.java

--- StriverAtoZ/5.Recursion/searching_and_sorting/Selectionsort.java
+++ StriverAtoZ/5.Recursion/searching_and_sorting/Selectionsort.java
@@ -1,43 +1,25 @@
-private static void merge(int[] arr, int low, int mid, int high) {
-    ArrayList<Integer> temp = new ArrayList<>(); // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
+//Problem link: https://www.geeksforgeeks.org/problems/selection-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=selection-sort
 
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.add(arr[left]);
-            left++;
-        } else {
-            temp.add(arr[right]);
-            right++;
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        int ans=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[ans]) ans=j;
         }
-    }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.add(arr[left]);
-        left++;
-    }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.add(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp.get(i - low);
-    }
-}
-
-public static void mergeSort(int[] arr, int low, int high) {
-    if (low >= high) return;
-    int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  // left half
-    mergeSort(arr, mid + 1, high); // right half
-    merge(arr, low, mid, high);  // merging sorted halves
+        return ans;
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    for(int i=0;i<arr.length-1;i++){
+	        int min_index=select(arr,i);
+	        if(min_index==i) continue;
+	        arr[min_index]=arr[min_index]+arr[i];
+	        arr[i]=arr[min_index]-arr[i];
+	        arr[min_index]=arr[min_index]-arr[i];
+	    }
+	    
+	}
 }
