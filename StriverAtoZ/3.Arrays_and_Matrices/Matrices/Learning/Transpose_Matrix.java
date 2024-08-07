33a352dfa2f9eef2ddd7dd92374b03c5911907bc
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-08-08 00:30:58 +0530

    new changes made

--- StriverAtoZ/3.Arrays_and_Matrices/Matrices/Learning/Transpose_Matrix.java
+++ StriverAtoZ/3.Arrays_and_Matrices/Matrices/Learning/Transpose_Matrix.java
@@ -14,3 +14,24 @@ class Solution
         }
     }
 }
+
+// Recursive Solution : https://www.geeksforgeeks.org/problems/swapping-triangles5209/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+class Solution {
+    int[][] swapTriangle(int N, int A[][]) {
+        outerloop(A,0);
+        return A;
+    }
+ private static void outerloop(int[][] matrix,int index){
+     if(index==matrix.length) return;
+     innerloop(matrix,index,index+1);
+     outerloop(matrix,index+1);
+ }   
+ private static void innerloop(int[][] matrix, int outer_iterator, int inner_iterator){
+     if(inner_iterator==matrix.length) return;
+     matrix[outer_iterator][inner_iterator]^=matrix[inner_iterator][outer_iterator];
+     matrix[inner_iterator][outer_iterator]^=matrix[outer_iterator][inner_iterator];
+     matrix[outer_iterator][inner_iterator]^=matrix[inner_iterator][outer_iterator];
+     innerloop(matrix,outer_iterator,inner_iterator+1);
+ }
+}
---
+++ StriverAtoZ/3.Arrays_and_Matrices/Matrices/Learning/Traverse.java
@@ -0,0 +1,8 @@
+// problem Link : https://www.geeksforgeeks.org/problems/boundary-traversal-of-matrix-1587115620/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+// Follow up : https://www.geeksforgeeks.org/problems/summed-matrix5834/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+// follow up2 : https://www.geeksforgeeks.org/problems/print-matrix-in-snake-pattern-1587115621/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+// follow up 3 : https://www.geeksforgeeks.org/problems/row-with-minimum-number-of-1s5430/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
