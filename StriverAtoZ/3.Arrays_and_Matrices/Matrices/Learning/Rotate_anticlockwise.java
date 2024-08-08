aa31c82529f9fcf31d98f5073e32ae3283c58e09
SaiPraveenKumar <saipraveenk816@gmail.com>
2024-08-08 19:15:10 +0530

    new changes made

--- StriverAtoZ/3.Arrays_and_Matrices/Matrices/Learning/Rotate_anticlockwise.java
+++ StriverAtoZ/3.Arrays_and_Matrices/Matrices/Learning/Rotate_anticlockwise.java
@@ -34,3 +34,54 @@ class Solution
 
 // Follow up Q : 180 degree rotation
 //https://www.geeksforgeeks.org/problems/c-matrix-rotation-by-180-degree0745/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+
+// Brute Force Solution
+class Solution
+{
+    public void rotate(int[][] matrix)
+    {
+        int n=matrix.length;
+        int[][] ans= new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans[n-i-1][n-j-1]=matrix[i][j];
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]= ans[i][j];
+            }
+        }
+    }
+}
+
+// Optimal Solution
+class Solution
+{
+    public void rotate(int[][] matrix)
+    {
+        for(int i=0;i<matrix.length/2;i++){
+            reverserows(matrix[i],matrix[matrix.length-i-1]);
+        }
+        for(int i=0;i<matrix.length;i++){
+            reverseelements(matrix[i]);
+        }
+    }
+    private static void reverserows(int[] arr1, int[] arr2){
+        for(int i=0;i<arr1.length;i++){
+            arr1[i]^=arr2[i];
+            arr2[i]^=arr1[i];
+            arr1[i]^=arr2[i];
+        }
+    }
+    private static void reverseelements(int[] nums){
+        int start=0,end=nums.length-1;
+        while(start<end){
+            nums[start]+=nums[end];
+            nums[end]=nums[start]-nums[end];
+            nums[start]-=nums[end];
+            start++;
+            end--;
+        }
+    }
+}
